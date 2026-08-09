cf92b71110a2952ba5c7d101462f366c716b8729
Yujin Jang <89734178+yujinway@users.noreply.github.com>
2026-08-09 23:15:37 +0900

    Tutorial 03 - Initializing DirectX 11

---
+++ Engine/applicationclass.cpp
@@ -0,0 +1,73 @@
+#include "applicationclass.h"
+
+
+ApplicationClass::ApplicationClass()
+{
+	m_Direct3D = 0;
+}
+
+
+ApplicationClass::ApplicationClass(const ApplicationClass& other)
+{
+}
+
+
+ApplicationClass::~ApplicationClass()
+{
+}
+
+
+bool ApplicationClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
+{
+	bool result;
+
+
+	m_Direct3D = new D3DClass;
+
+	result = m_Direct3D->Initialize(screenWidth, screenHeight, VSYNC_ENABLED, hwnd, FULL_SCREEN, SCREEN_DEPTH, SCREEN_NEAR);
+	if (!result)
+	{
+		MessageBox(hwnd, L"Could not initialize Direct3D", L"Error", MB_OK);
+		return false;
+	}
+
+	return true;
+}
+
+
+void ApplicationClass::Shutdown()
+{
+	if (m_Direct3D)
+	{
+		m_Direct3D->Shutdown();
+		delete m_Direct3D;
+		m_Direct3D = 0;
+	}
+
+	return;
+}
+
+
+bool ApplicationClass::Frame()
+{
+	bool result;
+
+
+	result = Render();
+	if (!result)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+
+bool ApplicationClass::Render()
+{
+	m_Direct3D->BeginScene(0.5f, 0.5f, 0.5f, 1.0f);
+
+	m_Direct3D->EndScene();
+
+	return true;
+}
---
+++ Engine/main.cpp
@@ -0,0 +1,23 @@
+#include "systemclass.h"
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
+{
+	SystemClass* System;
+	bool result;
+
+
+	System = new SystemClass;
+
+	result = System->Initialize();
+	if(result)
+	{
+		System->Run();
+	}
+
+	System->Shutdown();
+	delete System;
+	System = 0;
+
+	return 0;
+}
